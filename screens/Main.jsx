b51935a19739fffc44597630d3a2681d19f1bbf9
allforthe64 <williamhrainey@gmail.com>
2024-07-18 13:22:27 -0700

    added in react-native-safe-area-context

---
+++ screens/Main.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Main</Text>
+      </View>
+    )
+  }
+}
