d47e6c8884c1ff67a359fbf91139b4511210b2d2
allforthe64 <williamhrainey@gmail.com>
2025-01-02 13:50:50 -0800

    attempted to setup the appcheck custom provider on the mobile app

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,10 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 import firebase from 'firebase/compat/app'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform } from 'react-native'
 
+//appcheck imports
+import { initializeAppCheck, CustomProvider, getToken } from 'firebase/app-check';
 
 //configure firebase
 const firebaseConfig = {
@@ -13,7 +16,7 @@ const firebaseConfig = {
   projectId: "elephantapp-21e34",
   storageBucket: "elephantapp-21e34.appspot.com",
   messagingSenderId: "572276226266",
-  appId: "1:572276226266:web:e32e1b8d79b50c3cbd8de3",
+  appId: Platform.OS = 'android' ? "1:572276226266:android:5660015065e0d5b4bd8de3" : "1:572276226266:ios:c8422618b2f9d710bd8de3",
   measurementId: "G-V18K1CK9N5"
 };
   
@@ -22,6 +25,37 @@ const app = initializeApp(firebaseConfig);
 export const firebaseAuth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+
+// Generate your custom token
+const generateCustomToken = async () => {
+  const response = await fetch('https://www.myelephantapp.com/api/db-link', {
+    method: 'POST',
+    body: JSON.stringify({ appId: Platform.OS = 'android' ? '1:572276226266:android:5660015065e0d5b4bd8de3' : '1:572276226266:ios:c8422618b2f9d710bd8de3' }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const data = await response.json();
+  if (data.appCheckToken) {
+    return data.appCheckToken.token;
+  } else {
+    throw new Error("Token not received from backend");
+  }
+};
+
+// Initialize App Check with the custom provider
+const appCheck = initializeAppCheck(app, {
+  provider: new CustomProvider({
+    getToken: async () => {
+      const token = await generateCustomToken();
+      return {
+        token,
+        expireTimeMillis: Date.now() + 60 * 60 * 1000, // 1 hour expiration
+      };
+    },
+  }),
+  isTokenAutoRefreshEnabled: true, // Enable auto-refresh of tokens
+});
+
+
 export const db = getFirestore(app)
 export const storage = getStorage(app)
 
