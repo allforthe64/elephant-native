497fdb8f9312139e34428828e37fd4b293d1b49e
allforthe64 <williamhrainey@gmail.com>
2025-03-03 22:38:30 -0800

    returned the export to original version

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 functions = require('firebase-functions/v2')
 
-export const helloWorldV2  = functions.https.onRequest((request, response) => {
+exports.helloWorldV2  = functions.https.onRequest((request, response) => {
     return response.json({ message: 'hello, world!' })
 })
