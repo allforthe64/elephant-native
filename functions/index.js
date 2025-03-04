8f4bae8822e153f36f1a527dcd6f0ca00d9d769b
allforthe64 <williamhrainey@gmail.com>
2025-03-04 10:46:07 -0800

    reinitialized

--- functions/index.js
+++ functions/index.js
@@ -17,8 +17,3 @@ exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
-
-exports.helloWorldV2 = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
