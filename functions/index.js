ef68a5dfe93a7a49f044ec62a8c87a02d1dbab70
allforthe64 <williamhrainey@gmail.com>
2025-03-03 22:48:12 -0800

    attempting to enforce get request only

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
-functions = require('firebase-functions/v2')
+const functions = require('firebase-functions/v2');
+const { logger } = require('firebase-functions');
 
-exports.helloWorldV2  = functions.https.onRequest((request, response) => {
-    return response.json({ message: 'hello, world!' })
-})
+exports.helloWorldV2 = functions.https.onRequest((request, response) => {
+    if (request.method === 'GET') {
+        logger.info('GET request received!');
+        return response.json({ message: 'hello, world!' });
+    } else {
+        logger.warn('Unsupported request method!');
+        return response.status(405).json({ error: 'Method Not Allowed' });
+    }
+});
