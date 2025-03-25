303daf0d8ca20ca9eb613c3c6cfd586bc3262e75
allforthe64 <williamhrainey@gmail.com>
2025-03-24 20:48:47 -0700

    alerting errors when uploading documents/added in . to mark text files

--- screens/DashboardStackScreens/Notes.jsx
+++ screens/DashboardStackScreens/Notes.jsx
@@ -144,7 +144,7 @@ const Notepad = () => {
 
       //add an image into the file queue
       let queue = JSON.parse(await AsyncStorage.getItem('uploadQueue')) || []
-      queue.push({uri: ['txt'], filename: filename, finalDestination: finalDestination, noteBody: body})
+      queue.push({uri: ['.txt'], filename: filename, finalDestination: finalDestination, noteBody: body})
       await AsyncStorage.setItem('uploadQueue', JSON.stringify(queue))
 
       UploadQueueEmitter.emit('uploadQueueUpdated', queue)
--- screens/Main.jsx
+++ screens/Main.jsx
@@ -251,39 +251,43 @@ const Main = () => {
         type: 'success'
       })
     } else {
-      const blob = await new Promise(async (resolve, reject) => {
-        const xhr = new XMLHttpRequest()
-        xhr.onload = () => {
-        resolve(xhr.response) 
-        }
-        xhr.onerror = (e) => {
-            reject(new TypeError('Network request failed'))
-        }
-        xhr.responseType = 'blob'
-        xhr.open('GET', file.uri, true)
-        xhr.send(null)
-      })
-      const fileUri = `${currentUser.uid}/${file.filename}`
-      const fileRef = ref(storage, `${currentUser.uid}/${formattedDate}`)
-      const result = await uploadBytesResumable(fileRef, blob)
-      const uploadSize = result.metadata.size
-      
-      const reference = await addfile({
-        name: file.filename,
-        fileType: fileType,
-        size: uploadSize,
-        uri: fileUri,
-        user: currentUser,
-        version: 0,
-        timeStamp: `${formattedDate}`
-      }, file.finalDestination)
-
-      const updatedUser = {...userInst, fileRefs: [...userInst.fileRefs, reference], spaceUsed: userInst.spaceUsed + uploadSize}
-      updateUser(updatedUser)
-
-      toast.show('Upload successful', {
-        type: 'success'
-      })
+      try {
+        const blob = await new Promise(async (resolve, reject) => {
+          const xhr = new XMLHttpRequest()
+          xhr.onload = () => {
+          resolve(xhr.response) 
+          }
+          xhr.onerror = (e) => {
+              reject(new TypeError('Network request failed'))
+          }
+          xhr.responseType = 'blob'
+          xhr.open('GET', file.uri, true)
+          xhr.send(null)
+        })
+        const fileUri = `${currentUser.uid}/${file.filename}`
+        const fileRef = ref(storage, `${currentUser.uid}/${formattedDate}`)
+        const result = await uploadBytesResumable(fileRef, blob)
+        const uploadSize = result.metadata.size
+        
+        const reference = await addfile({
+          name: file.filename,
+          fileType: fileType,
+          size: uploadSize,
+          uri: fileUri,
+          user: currentUser,
+          version: 0,
+          timeStamp: `${formattedDate}`
+        }, file.finalDestination)
+  
+        const updatedUser = {...userInst, fileRefs: [...userInst.fileRefs, reference], spaceUsed: userInst.spaceUsed + uploadSize}
+        updateUser(updatedUser)
+  
+        toast.show('Upload successful', {
+          type: 'success'
+        })
+      } catch (err) {
+        alert(err)
+      }
     }
 
   }
