12759c18bd8ce91e7179b42f63767386e7c3e359
allforthe64 <williamhrainey@gmail.com>
2025-10-14 15:44:03 -0700

    scaffold out new auth page

--- screens/Auth.jsx
+++ screens/Auth.jsx
@@ -4,6 +4,9 @@ import { firebaseAuth } from '../firebaseConfig'
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 import { getUser } from '../firebase/firestore'
 
+//useToast import for displaying notifications
+import { useToast } from 'react-native-toast-notifications'
+
 const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 const Login = ({navigation: {navigate}}) => {
@@ -14,6 +17,9 @@ const Login = ({navigation: {navigate}}) => {
     const [validEmail, setValidEmail] = useState(false)
     const [signUpMode, setSignUpMode] = useState(false)
     const auth = firebaseAuth
+
+    //consume toast context for notifications
+    const toast = useToast()
 
     useEffect(() => {
         const emailResult = EMAIL_REGEX.test(userEmail)
@@ -36,72 +42,81 @@ const Login = ({navigation: {navigate}}) => {
         }
     }
 
-    const signUp = async () =>{
-        if (password !== passwordConf || !validEmail || userEmail === '') {
-            alert('Sign up error! Make sure you entered your details correctly!')
-        }
-        try {
-            const response = await createUserWithEmailAndPassword(auth, userEmail, password)
-            console.log(response)
-            const userData = await getUser(response._tokenResponse)
-            alert('Check your emails!')
-        } catch (err) {
-            console.log(err)
-            alert('Sign Up Failed: ', err.message)
-            return
-        } finally {
-            setLoading(false)
-            setUserEmail('')
-            setPassword('')
-            setSignUpMode(false)
-            navigate('Registration Complete')
-        }
+    const sendRegistrationLink = async () => {
+        const response = await fetch('www.myelephantapp.com/send-registration-email', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: {
+                to_email: userEmail
+            }
+        })
+
+        toast.show(`Registration link sent. Check your email!`, {
+            type: 'success'
+        })
+        setUserEmail('')
+        setSignUpMode(false)
     }
 
 
   return (
     <KeyboardAvoidingView style={{width: '100%', height: '100%', backgroundColor: 'black', position: 'relative'}} enabled={true} behavior='padding'>
-                <View style={styles.innerContainer}>
-                    <Text style={styles.bigHeader}>{signUpMode ? 'Register account' : 'Sign in'}</Text>
-                    <View style={styles.formCon}>
-                        <Text style={styles.subheading}>Enter Email:</Text>
-                        <TextInput style={(validEmail || userEmail === '') ? styles.input : styles.inputInvalid} placeholder='Enter Email' autoCapitalize='none' placeholderTextColor={'rgb(0, 0, 0)'} value={userEmail} onChangeText={(e) => setUserEmail(e)}/>
-                        <Text style={(validEmail || userEmail === '') ? {display: 'none'} : styles.invalid}>Please Enter A Valid Email</Text>
-                        <Text style={styles.subheading}>Enter Password:</Text>
-                        <TextInput secureTextEntry={true} style={styles.input} placeholder='Enter Password' placeholderTextColor={'rgb(0, 0, 0)'} value={password} onChangeText={(e) => setPassword(e)}/>
-                        {signUpMode && 
-                            <>
-                                <Text style={styles.subheading}>Confirm Password:</Text>
-                                <TextInput secureTextEntry={true} style={styles.input} placeholder='Confirm Password' placeholderTextColor={'rgb(0, 0, 0)'} value={passwordConf} onChangeText={(e) => setPasswordConf(e)}/>
-                                <Text style={((password.length > 0 && passwordConf.length > 0) && (password === passwordConf)) ? {display: 'none'} : styles.invalid}>Passwords Do Not Match</Text>
-                            </>
-                        }
-                    </View>
-                </View>
-                <View style={styles.wrapperContainer}>
-                        <View style={userEmail === '' || !validEmail || password !== passwordConf ? styles.buttonWrapperDisabled : styles.buttonWrapper}>
-                            {signUpMode ? 
-                                <TouchableOpacity onPress={() => signUp()} disabled={!validEmail || userEmail === '' || password !== passwordConf ? true : false}>
-                                    <Text style={styles.inputButton}>Register</Text>
-                                </TouchableOpacity>
-                            :
-                            <TouchableOpacity onPress={() => login()}>
-                                <Text style={styles.inputButton}>Sign In</Text>
-                            </TouchableOpacity>
-                            }
+
+                {!signUpMode ?
+                    <>
+                        <View style={styles.innerContainer}>
+                            <Text style={styles.bigHeader}>{signUpMode ? 'Register account' : 'Sign in'}</Text>
+                            <View style={styles.formCon}>
+                                <Text style={styles.subheading}>Enter Email:</Text>
+                                <TextInput style={(validEmail || userEmail === '') ? styles.input : styles.inputInvalid} placeholder='Enter Email' autoCapitalize='none' placeholderTextColor={'rgb(0, 0, 0)'} value={userEmail} onChangeText={(e) => setUserEmail(e)}/>
+                                <Text style={(validEmail || userEmail === '') ? {display: 'none'} : styles.invalid}>Please Enter A Valid Email</Text>
+                                <Text style={styles.subheading}>Enter Password:</Text>
+                                <TextInput secureTextEntry={true} style={styles.input} placeholder='Enter Password' placeholderTextColor={'rgb(0, 0, 0)'} value={password} onChangeText={(e) => setPassword(e)}/>
+                            </View>
                         </View>
-                        <Text style={{color: 'white', width: '80%', textAlign: 'center'}}>
-                            {signUpMode ? "Already have an account?" : "Don't have an Account?"}
-                            <TouchableOpacity onPress={() => {
-                                    setUserEmail('')
-                                    setPassword('')
-                                    setSignUpMode(prev => !prev)
-                                }}>
-                                <Text style={styles.inputText}> Click here </Text>
-                            </TouchableOpacity>
-                            {signUpMode ? "to login." : "to Register for a new one."}
-                        </Text>
-                    </View>
+                        <View style={styles.wrapperContainer}>
+                                <View style={userEmail === '' || !validEmail || password === '' ? styles.buttonWrapperDisabled : styles.buttonWrapper}>
+                                    <TouchableOpacity onPress={() => login()}>
+                                        <Text style={styles.inputButton}>Sign In</Text>
+                                    </TouchableOpacity>
+                                </View>        
+                        </View>
+                    </>
+                :
+                    <>
+                        <View style={styles.innerContainer}>
+                            <Text style={styles.bigHeader}>Get started:</Text>
+                            <Text style={styles.subheading}>Enter your email to get a registration link:</Text>
+                            <View style={styles.registerFormCon}>
+                                <Text style={styles.subheading}>Enter Email:</Text>
+                                <TextInput style={(validEmail || userEmail === '') ? styles.input : styles.inputInvalid} placeholder='Enter Email' autoCapitalize='none' placeholderTextColor={'rgb(0, 0, 0)'} value={userEmail} onChangeText={(e) => setUserEmail(e)}/>
+                                <View style={styles.wrapperContainer}>
+                                    <View style={userEmail === '' || !validEmail ? styles.buttonWrapperDisabled : styles.buttonWrapper}>
+                                        <TouchableOpacity onPress={() => sendRegistrationLink()}>
+                                            <Text style={styles.inputButton}>Send link</Text>
+                                        </TouchableOpacity>
+                                    </View>        
+                                </View>
+                            </View>
+                        </View>
+                    </>
+                }
+
+                <View style={styles.wrapperContainer}>
+                    <Text style={{color: 'white', width: '80%', textAlign: 'center'}}>
+                        {signUpMode ? "Already have an account?" : "Don't have an Account?"}
+                        <TouchableOpacity onPress={() => {
+                                setUserEmail('')
+                                setPassword('')
+                                setSignUpMode(prev => !prev)
+                            }}>
+                            <Text style={styles.inputText}> Click here </Text>
+                        </TouchableOpacity>
+                        {signUpMode ? "to login." : "to Register for a new one."}
+                    </Text>
+                </View>
     </KeyboardAvoidingView>
   )
 }
@@ -137,6 +152,15 @@ const styles = StyleSheet.create({
         marginBottom: '10%',
         display: 'flex',
         alignItems: 'center',
+    },
+    registerFormCon: {
+        width: '100%',
+        height: '60%',
+        marginBottom: '10%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     wrapperContainer: {
         display: 'flex',
