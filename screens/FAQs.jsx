79936780cd98cece01d0d784cbd60e5ca77536d5
allforthe64 <williamhrainey@gmail.com>
2026-05-17 21:57:59 -0700

    making sure to import touchable opacity

--- screens/FAQs.jsx
+++ screens/FAQs.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { ScrollView, View, Text, StyleSheet } from 'react-native'
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { List } from 'react-native-paper'
 
 import { useNavigation } from '@react-navigation/native'
